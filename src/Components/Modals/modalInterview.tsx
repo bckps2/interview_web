320ee4e3753d11a85eee421d7d25b2718ffd25c8
bckps2 <bckfl10@gmail.com>
2022-08-11 01:49:10 +0200

    [Feature01] - fixing models

---
+++ src/Components/Modals/modalInterview.tsx
@@ -0,0 +1,34 @@
+import { Button, Form } from "react-bootstrap";
+import { InterviewForm } from "../Forms/views/interviewForm";
+
+interface propsProcess {
+    idInterview: number,
+    submit: any
+}
+
+export const ModalInterview = ({ submit, idInterview }: propsProcess) => {
+
+    return (
+        <div className="modal fade" data-bs-toggle="modal" data-bs-backdrop="false" id={"interview"+ idInterview + "Modal"} tabIndex={-1} aria-labelledby={"exampleModalLabel"} >
+            <div className="modal-dialog" >
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div className="modal-body">
+                        <Form  onSubmit={submit}>
+                            <InterviewForm />
+                            <input type="hidden" value={idInterview} name="idInterview"></input>
+                            <Button type="submit">Añadir entrevista </Button>
+                        </Form>
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
